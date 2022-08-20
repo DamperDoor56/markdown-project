433878875f5a737648bedf1dda3440fae4a75149
Ludmi Lopez <ludmit@outlook.com>
2022-08-20 17:11:00 -0300

    need-to-add-yarn-fix

--- src/App.js
+++ src/App.js
@@ -1,35 +1,38 @@
 import './styles/App.scss';
-import React, {useState} from 'react';
-import ReactMarkdown from 'react-markdown';
-import SyntaxHighlighter from 'react-syntax-highlighter';
-import ReactDOM from 'react-dom'
-import {atomOneDark} from 'react-syntax-highlighter/dist/esm/styles/hljs'
+import React from 'react';
 
+var Prism = document.createElement('script');  
+Prism.setAttribute('src','https://cdnjs.cloudflare.com/ajax/libs/prism/9000.0.1/prism.min.js');
+document.head.appendChild(Prism);
 
+const marked = require("marked");
 
-function App() {
-  const [input, setInput] = useState();
-  return(
-    <div className='App'>
-      <textarea autoFocus className='textarea' value={input} onChange={(e) => setInput(e.target.value)} />
-
-      <ReactMarkdown 
-      children={input}
-      className='markdown' 
-      style={atomOneDark}
-      renderers ={{
-        code: Component,
-      }}/>
-    </div>
-  )
-}
-
-const Component = ({value, language}) =>{
-  return(
-    <SyntaxHighlighter language={language ?? null} style={atomOneDark}>
-      {value ?? ''}
-    </SyntaxHighlighter>
-  );
+marked.setOptions({
+  breaks: true,
+  highlight: function (code) {
+      return Prism.highlight(code, Prism.languages.javascript, 'javascript');
+    }
+});
+const renderer = new marked.Renderer();
+renderer.link = function(href, title, text){
+  return `<a target="_blank" href="${href}">${text}</a>`;
 };
 
+const Editor = ({ content, textareaChange }) => <textarea value={content} onChange={textareaChange}/>
+
+const Previewer = ({ content }) => <div id='preview'>{content}</div> 
+
+function App() {
+ const [content, setContent] = React.useState('Hey This is Mohammed')
+
+ const textareaChange= (event) => {
+  setContent(event.target.value)
+ }
+  return(
+   <div className='main'>
+    <Editor content={content} textareaChange={textareaChange}/>
+    <Previewer content={content} />
+   </div>
+  )
+}
 export default App;
