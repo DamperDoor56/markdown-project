638f7d62daa097558b5df3923cbccfe3ecc102d7
Ludmi Lopez <ludmit@outlook.com>
2022-08-20 12:21:06 -0300

    need-fix-SyntaxHighlighter

--- src/App.js
+++ src/App.js
@@ -1,38 +1,35 @@
-import './App.scss';
+import './styles/App.scss';
 import React, {useState} from 'react';
 import ReactMarkdown from 'react-markdown';
 import SyntaxHighlighter from 'react-syntax-highlighter';
-import { docco } from 'react-syntax-highlighter/dist/esm/styles/hljs';
+import ReactDOM from 'react-dom'
+import {atomOneDark} from 'react-syntax-highlighter/dist/esm/styles/hljs'
 
 
 
 function App() {
-  const [input, setInput] = useState('');
-  return (
-    <div className="App">
-      <div>
-        <textarea autoFocus className='textarea' value={input} onChange={(e) => setInput(e.target.value)} />
-      </div>
-      <ReactMarkdown
-      source={input}
-      className='markdown'
-      renderers={{
-        code:Component,
-      }}
-      >{input}</ReactMarkdown>
-
-    </div>
-  );
-}
-
-
-
-const Component = ({ value, language }) => {
+  const [input, setInput] = useState();
   return(
-    <SyntaxHighlighter language={language ?? null} style={docco}>
-      {value ?? ''}
-    </SyntaxHighlighter>
+    <div className='App'>
+      <textarea autoFocus className='textarea' value={input} onChange={(e) => setInput(e.target.value)} />
+
+      <ReactMarkdown 
+      children={input}
+      className='markdown' 
+      style={atomOneDark}
+      renderers ={{
+        code: Component,
+      }}/>
+    </div>
   )
 }
+
+const Component = ({value, language}) =>{
+  return(
+    <SyntaxHighlighter language={language ?? null} style={atomOneDark}>
+      {value ?? ''}
+    </SyntaxHighlighter>
+  );
+};
 
 export default App;
