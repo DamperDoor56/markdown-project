d6d129266db5da80968b75ba8c876f4d524cf1f7
Ludmi Lopez <ludmit@outlook.com>
2022-08-20 17:24:56 -0300

    Functionality

--- src/App.js
+++ src/App.js
@@ -1,37 +1,14 @@
 import './styles/App.scss';
-import React from 'react';
-
-var Prism = document.createElement('script');  
-Prism.setAttribute('src','https://cdnjs.cloudflare.com/ajax/libs/prism/9000.0.1/prism.min.js');
-document.head.appendChild(Prism);
-
-const marked = require("marked");
-
-marked.setOptions({
-  breaks: true,
-  highlight: function (code) {
-      return Prism.highlight(code, Prism.languages.javascript, 'javascript');
-    }
-});
-const renderer = new marked.Renderer();
-renderer.link = function(href, title, text){
-  return `<a target="_blank" href="${href}">${text}</a>`;
-};
-
-const Editor = ({ content, textareaChange }) => <textarea value={content} onChange={textareaChange}/>
-
-const Previewer = ({ content }) => <div id='preview'>{content}</div> 
+import React, {useState} from 'react';
+import { marked } from 'marked';
 
 function App() {
- const [content, setContent] = React.useState('Hey This is Mohammed')
-
- const textareaChange= (event) => {
-  setContent(event.target.value)
- }
+ const [markdown, setMarkdown] = useState('');
+ 
   return(
    <div className='main'>
-    <Editor content={content} textareaChange={textareaChange}/>
-    <Previewer content={content} />
+   <textarea  value={markdown} onChange={e => setMarkdown(e.target.value)}></textarea>
+   <div dangerouslySetInnerHTML={{ __html: marked(markdown) }} className="output"></div>
    </div>
   )
 }
